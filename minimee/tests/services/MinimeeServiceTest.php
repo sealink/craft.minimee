32badc59b63c87e3117977933ffaad51ec7232fe
John D Wells <johndwells@gmail.com>
2014-04-05 22:36:23 +0100

    Refactor init() to reduce to only what we need (at this point in our tests).

--- minimee/tests/services/MinimeeServiceTest.php
+++ minimee/tests/services/MinimeeServiceTest.php
@@ -7,35 +7,55 @@ class MinimeeServiceTest extends BaseTest
 {
 	public function setUp()
 	{
-		require_once __DIR__ . '/../vendor/autoload.php';
-		require_once __DIR__ . '/../../library/vendor/autoload.php';
-		require_once __DIR__ . '/../../MinimeePlugin.php';
-		require_once __DIR__ . '/../../services/MinimeeService.php';
+		$this->autoload();
 
-        $this->config = m::mock('Craft\ConfigService');
-        $this->config->shouldReceive('getIsInitialized')->andReturn(true);
-        $this->config->shouldReceive('usePathInfo')->andReturn(true)->byDefault();
+        // $this->config = m::mock('Craft\ConfigService');
+        // $this->config->shouldReceive('getIsInitialized')->andReturn(true);
+        // $this->config->shouldReceive('get')->with('usePathInfo')->andReturn(true)->byDefault();
+        // $this->config->shouldReceive('get')->with('translationDebugOutput')->andReturn(false)->byDefault();
+        // $this->config->shouldReceive('get')->with('resourceTrigger')->andReturn('resource')->byDefault();
+        // $this->config->shouldReceive('get')->with('version')->andReturn('2.0');
 
-        $this->setComponent(craft(), 'config', $this->config);
+        // $this->setComponent(craft(), 'config', $this->config);
 
 		minimee()->stash('plugin', new MinimeePlugin);
 		minimee()->stash('service', new MinimeeService);
 
+		// TODO: figure outo how to propery mock config so that we can run init()
 		//minimee()->service->init();
 	}
 
-	public function dataProviderInValidUrls()
+	public function autoload()
+	{
+		// our tests use this
+		require_once __DIR__ . '/../../library/vendor/autoload.php';
+
+		// These are usually automatically loaded by Craft
+		require_once __DIR__ . '/../../MinimeePlugin.php';
+		require_once __DIR__ . '/../../services/MinimeeService.php';
+
+		// this usually happens in MinimeePlugin::init()
+		require_once __DIR__ . '/../vendor/autoload.php';
+
+		// And these Craft can usually autoload
+		// require_once __DIR__ . '/../../models/Minimee_AssetBaseModel.php';
+		// require_once __DIR__ . '/../../models/Minimee_LocalAssetModel.php';
+		// require_once __DIR__ . '/../../models/Minimee_RemoveAssetModel.php';
+		// require_once __DIR__ . '/../../models/Minimee_SettingsModel.php';
+	}
+
+	public function dataProviderInvalidUrls()
 	{
 		return [
 			['domain.com'],
-			// ['/domain.com']
+			['/domain.com']
 		];
 	}
 
 	/**
-	 * @dataProvider dataProviderInValidUrls
+	 * @dataProvider dataProviderInvalidUrls
 	 */
-	public function testIsUrlInValid($url)
+	public function testIsUrlInvalid($url)
 	{
 		$isUrl = $this->getMethod(minimee()->service, 'isUrl');
 		$this->assertSame(false, $isUrl->invokeArgs(minimee()->service, array($url)));
@@ -45,8 +65,8 @@ class MinimeeServiceTest extends BaseTest
 	{
 		return [
 			['http://domain.com'],
-			// ['https://domain.com'],
-			// ['//domain.com']
+			['https://domain.com'],
+			['//domain.com']
 		];
 	}
 
